44b7706f1e4e2b0f886dd6812f84801271769ce5
MD Nasirul Islam <71561580+KingNasirul@users.noreply.github.com>
2024-08-14 04:44:57 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;
+    alert(`Login with Email: ${email} and Password: ${password}`);
+});
+
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('signupName').value;
+    const email = document.getElementById('signupEmail').value;
+    const password = document.getElementById('signupPassword').value;
+    alert(`Sign Up with Name: ${name}, Email: ${email} and Password: ${password}`);
+});
